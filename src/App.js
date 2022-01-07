905880ff48f6d369797cfba6207c4628342d0b6b
chandrakant-karole <chandrakantkarole001995@gmail.com>
2022-01-07 18:03:37 +0530

    custom loader added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import "aos/dist/aos.css";
 import './assets/css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {React, useState, useEffect} from "react";
 import Home from './pages/Home'
+import Loader from "./Loader";
 import { Route, Routes } from 'react-router-dom'
+
+
 function App() {
+  const [page, setPage] = useState(<Loader/>)
+
+  useEffect(() => {
+    let initiateLoader = setTimeout(()=>{
+      setPage(<Home/>)
+    }, 3000)
+
+    return (()=>{
+
+      clearTimeout(initiateLoader)
+    })
+  }, [])
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={page}/>
       </Routes>
     </>
   );
---
+++ src/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Loader() {
+    return (
+        <>
+            <div className="loaderContainer">
+                <div className='main_loader'>
+                    <div className='inner_loader'></div>
+                </div>
+            </div>
+        </>
+    )
+}
